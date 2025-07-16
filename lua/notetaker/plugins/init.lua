db73eb37b1386f0ca6d400e819b01f16a0af3026
Saddat Ahmad <enhancedtechnologies24@gmail.com>
2025-07-16 10:26:38 -0500

    update submodule

--- lua/notetaker/plugins/init.lua
+++ lua/notetaker/plugins/init.lua
@@ -4,5 +4,9 @@ vim.g.loaded_netrwPlugin = 1
 return {
 	"nvim-lua/plenary.nvim", -- lua functions that many plugins use
 	{ import = "notetaker.plugins.ui.nvim-tree" },
+	{
+		"grapp-dev/nui-components.nvim",
+		dependencies = { "MunifTanjim/nui.nvim" }
+	}
 }
 
